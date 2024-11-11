61ee6eecdd6ddd1c1fbe772fd50b09169ad72994
pintosum <oldogg@yandex.ru>
2024-11-11 21:48:00 +0300

    added formatters

---
+++ lua/pintosum/lazy/null_ls.lua
@@ -0,0 +1,15 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.eslint,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.blue,
+        null_ls.builtins.formatting.clang_format,
+      }
+    })
+  end
+}
