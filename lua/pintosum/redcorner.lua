900314a961b748f82c5b1efb7f8c42b670a9b325
Pinto Sumeragi <pintosum@ev-q4.kzn.21-school.ru>
2024-12-24 20:40:55 +0300

    -

--- lua/pintosum/redcorner.lua
+++ lua/pintosum/redcorner.lua
@@ -1,18 +1,12 @@
-
 local function create_float()
-  local win = vim.api.nvim_list_uis()
-
   local width = 12
-
-  local height = 10
+  local height = 8
   local bufnr = vim.api.nvim_create_buf(false, true)
   local win_id = vim.api.nvim_open_win(bufnr, true, {
     relative = "editor",
-    --title = "Redcorner",
-    --title_pos = "center",
     --row = math.floor(((vim.o.lines - height) / 2) - 1),
     --col = math.floor((vim.o.columns - width) / 2),
-    row = 0,
+    row = vim.o.lines - 10,
     col = vim.o.columns - 1,
     width = width,
     height = height,
@@ -34,8 +28,19 @@ local function create_float()
   return win_id, bufnr
 end
 
-local win_id, bufnr = create_float()
+local function chr_tree()
+  local win_id, bufnr = create_float()
+  vim.fn.win_execute(win_id, "CellularAutomaton tree")
+end
 
-vim.fn.win_execute(win_id, "CellularAutomaton tree")
+chr_tree()
 
+vim.api.nvim_create_autocmd('WinResized', {
+  group = vim.api.nvim_create_augroup('pintosum', {}),
+  pattern = '*',
+  callback = function()
+    vim.cmd("fclose")
+    chr_tree()
+  end
+})
 
